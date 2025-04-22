6851c93f900d854291f39817fdfea73e42b7cf74
Carlos Azocar Riquelme <78894939+Carlos-Azocar-Riquelme@users.noreply.github.com>
2025-04-21 21:51:30 -0400

    agrega whitenoise

--- SMA/SMA/settings.py
+++ SMA/SMA/settings.py
@@ -53,10 +53,12 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'drf_spectacular',
     'plan.apps.PlanConfig',
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,11 +155,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Configuración para servir archivos estáticos en producción
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- build.sh
+++ build.sh
@@ -5,10 +5,14 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Create static directory if it doesn't exist
+mkdir -p static
+
 # Convert static asset files
-python SMA/manage.py collectstatic --no-input
+python SMA/manage.py collectstatic --no-input --clear
 
 # Apply any outstanding database migrations
 python SMA/manage.py migrate
 
-# python -m gunicorn myproject.asgi:application -k uvicorn.workers.UvicornWorker
+# Start the application with Gunicorn
+# cd SMA && python -m gunicorn SMA.asgi:application -k uvicorn.workers.UvicornWorker
